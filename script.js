8576a8448ad489763e35d2e444bc5751708a00cc
saicoder33 <saidighe731@gmail.com>
2026-06-05 23:31:20 +0530

    Changed-7

--- script.js
+++ script.js
@@ -122,3 +122,10 @@ if (container) {
 
     updateStars();
 }
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
