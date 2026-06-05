f96e9c576e4a982c562efd52ebc207d22fa18e96
saicoder33 <saidighe731@gmail.com>
2026-06-05 23:44:15 +0530

    Humburger menu-1

--- script.js
+++ script.js
@@ -125,7 +125,16 @@ if (container) {
 
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
+const links = document.querySelectorAll(".nav-links a");
 
+// Toggle menu when hamburger clicked
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+// Close menu when any link is clicked
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
